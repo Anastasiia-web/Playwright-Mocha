de6fbf5129296c6df2174d9cc45121bf8d2aa08c
Anastasiia <anastasiyamarket@gmail.com>
2022-12-11 14:54:07 +0100

    Mocha integration with Playwright test

--- tests/mocha.spec.js
+++ tests/mocha.spec.js
@@ -1,8 +1,29 @@
 const { chromium } = require('playwright')
-const expect = require('chai').expect
+const expect = require('chai').expect                                    // chai library for creating assertions
 
 let browser
+let page
 
 before(async () => {
-    await browser.launch()
+    browser = await chromium.launch()
 })
+
+after(async () => {
+    await browser.close()
+})
+
+beforeEach(async () => {
+    page = await browser.newPage()
+})
+
+afterEach(async () => {
+    await page.close()
+})
+
+it('Verifying that header is displayed', async () => {
+    await page.goto('https://www.example.com')
+    expect('h1'.to.exist)
+})
+
+
+// npm run test                                                                   // script is written in 'package.json' file
